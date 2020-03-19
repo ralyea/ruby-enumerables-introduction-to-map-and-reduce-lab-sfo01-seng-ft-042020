7903688a863e7da2feb0260779f3eef86d700bfe
Paul Ralyea <047par@gmail.com>
2020-03-19 01:43:36 +0000

    Automatically backed up by Learn

--- lib/my_code.rb
+++ lib/my_code.rb
@@ -14,3 +14,11 @@ end
 def map_to_square(source_array)
 (source_array).map { |i| i*i }
 end
+
+def reduce_to_total(source_array, starting_point)
+  
+  
+def reduce_to_all_true(source_array)
+
+
+def reduce_to_any_true(source_array)
