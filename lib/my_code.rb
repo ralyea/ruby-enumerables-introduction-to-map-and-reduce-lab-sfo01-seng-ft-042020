0c3ef140000ab63b769cb1bde0d86d0051fb9c42
Paul Ralyea <047par@gmail.com>
2020-03-19 01:40:39 +0000

    Automatically backed up by Learn

--- lib/my_code.rb
+++ lib/my_code.rb
@@ -4,7 +4,6 @@ def map_to_negativize(source_array)
 end
 
 
-#new_numbers = numbers.map { |number| number = number + 1 }
 (["paul", "gurney", "vladimir", "jessica", "chani"]).map { |i| i }
 ([1, 2, 3, -9]).map { |i| i*2 }
 ([1, 2, 3, -9]).map { |i| i*i }
