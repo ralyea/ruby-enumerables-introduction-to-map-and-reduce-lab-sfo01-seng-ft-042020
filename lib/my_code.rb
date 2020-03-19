144d57870807a300c294c24c172c23f43e5e3a95
Paul Ralyea <047par@gmail.com>
2020-03-19 01:52:59 +0000

    Automatically backed up by Learn

--- lib/my_code.rb
+++ lib/my_code.rb
@@ -14,18 +14,35 @@ end
 def map_to_square(source_array)
 (source_array).map { |i| i*i }
 end
-777777
 
-
-def reduce_to_total(source_array, starting_point=0)
-  if starting_point != 0 
-    array.reduce(starting_point){|x,y| :+ } 
-  else 
-    array.reduce{|x,y| yield x,y}
+def reduce_to_total(source_array, starting_point = 0)
+  total = starting_point
+  index = 0 
+  while index < source_array.count do 
+    total += source_array[index]
+    index += 1 
   end
+  total
 end
-  
-def reduce_to_all_true(source_array)
 
+def reduce_to_all_true(source_array)
+  index = 0
+  while index < source_array.count do
+    if !source_array[index]
+      return FALSE
+    end
+    index += 1 
+  end
+  return TRUE
+end
 
 def reduce_to_any_true(source_array)
+  index = 0 
+  while index < source_array.count do 
+    if source_array[index]
+      return TRUE
+    end
+    index += 1 
+  end
+  return FALSE
+end
