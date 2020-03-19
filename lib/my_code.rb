05f2f090d56ac2075e9cfeffb59fb65e7afeeb35
Paul Ralyea <047par@gmail.com>
2020-03-19 01:23:54 +0000

    Automatically backed up by Learn

--- lib/my_code.rb
+++ lib/my_code.rb
@@ -1,5 +1,5 @@
 # My Code here....
-map_to_negativize(source_array).map { |i| -i }
-map_to_no_change(source_array).map { |i| i }
-map_to_double(source_array).map { |i| i*2 }
-map_to_square(source_array).map { |i| i*i }
+([1, 2, 3, -9]).map { |i| -i }
+([1, 2, 3, -9]).map { |i| i }
+([1, 2, 3, -9]).map { |i| i*2 }
+([1, 2, 3, -9]).map { |i| i*i }
