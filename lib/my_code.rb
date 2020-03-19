cdb73f7074dff095eff90a487437f37efe182bcb
Paul Ralyea <047par@gmail.com>
2020-03-19 01:21:39 +0000

    Automatically backed up by Learn

--- lib/my_code.rb
+++ lib/my_code.rb
@@ -1,1 +1,5 @@
 # My Code here....
+map_to_negativize(source_array).map { |i| -i }
+map_to_no_change(source_array).map { |i| i }
+map_to_double(source_array).map { |i| i*2 }
+map_to_square(source_array).map { |i| i*i }
