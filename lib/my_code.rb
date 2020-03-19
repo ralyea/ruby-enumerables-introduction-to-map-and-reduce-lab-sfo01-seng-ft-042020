a8f7e89630b9e99e53c95b38059cc053c47636cb
Paul Ralyea <047par@gmail.com>
2020-03-19 01:42:16 +0000

    Automatically backed up by Learn

--- lib/my_code.rb
+++ lib/my_code.rb
@@ -7,6 +7,9 @@ def map_to_no_change(source_array)
  (source_array).map { |i| i }
  end
  
+def map_to_double(source_array)
+(source_array).map { |i| i*2 }
+end
 
-([1, 2, 3, -9]).map { |i| i*2 }
+
 ([1, 2, 3, -9]).map { |i| i*i }
