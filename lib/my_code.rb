028842938de2676a7781f29e3ebab81f7cf5af6e
Paul Ralyea <047par@gmail.com>
2020-03-19 01:37:06 +0000

    Automatically backed up by Learn

--- lib/my_code.rb
+++ lib/my_code.rb
@@ -4,6 +4,10 @@ new_array = (source_array).map { |i| -i }
 end
 puts new_array
 end
+array = [1,2,3]
+array.map { |n| n * 2 }
+
+
 #new_numbers = numbers.map { |number| number = number + 1 }
 (["paul", "gurney", "vladimir", "jessica", "chani"]).map { |i| i }
 ([1, 2, 3, -9]).map { |i| i*2 }
