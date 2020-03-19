0ef32da5efdd41321a0d15e2af4f00ac474f40ed
Paul Ralyea <047par@gmail.com>
2020-03-19 01:24:20 +0000

    Automatically backed up by Learn

--- lib/my_code.rb
+++ lib/my_code.rb
@@ -1,5 +1,5 @@
 # My Code here....
 ([1, 2, 3, -9]).map { |i| -i }
-([1, 2, 3, -9]).map { |i| i }
+(["paul", "gurney", "vladimir", "jessica", "chani"]).map { |i| i }
 ([1, 2, 3, -9]).map { |i| i*2 }
 ([1, 2, 3, -9]).map { |i| i*i }
